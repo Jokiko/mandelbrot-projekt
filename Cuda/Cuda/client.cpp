0664eab755229e89b985afba99e1c8fe4f78ea56
Randy Stoppe <randy.stoppe@gmail.com>
2021-02-09 02:31:23 +0100

    Revert "Client Klasse + Header erstellt"
    This reverts commit 6f98f9f15f385527e2aec34fa4eca3d3f596a911.

--- Cuda/Cuda/client.cpp
+++ Cuda/Cuda/client.cpp
@@ -1,132 +1,70 @@
 #include <stdio.h>
 #include <windows.h>
-
 #pragma comment( lib, "ws2_32.lib" )
-#define PORT 5000 //default server port 
-#define BUFLEN 1024 //defaul bufferlength for messages
-
-/*
-Client class handles the communication with our Javaserver
-
-*/
+#define PORT 5000
 
 
-class Client {
+using namespace std;
 
-public:
-	Client();
-	void sendMessage(const char* message);
-	void receiveMessage(char* receiveBuf, int len);
-	void close();
+int startWinsock()
+{
 
-private:
-	
-	SOCKET s_socket;
-	SOCKADDR_IN addr;
 	WSADATA wsaData;
-	
-	const char* FIRST_CONTACT;
-	char recvbuf[BUFLEN];
+	return WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+}
+
+int main(int argc, char const* argv[]) 
+{
+
+
+	// Initializing Winsocket
+
 	int iResult;
+	iResult = startWinsock();
 
-	void startWinsock();
-	void checkSendError(int ec);
-	void checkReceiveError(int ec);
-	void createSocket(); 
-	void connectToServer();
-	
-};
-
-Client::Client() {
-	startWinsock();
-	createSocket();
-	connectToServer();
-};
-
-void Client::startWinsock() {
-
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-	if (iResult != 0)
+	if (iResult != 0) 
 	{
 		printf("WSAStartup failed: %d\n", iResult);
 		WSACleanup(); // Cleanup after error
-		exit(1);
+		return 1;
 	}
 
-};
+	//Creating Socket
 
-void Client::checkSendError(int ec) {
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (ec == SOCKET_ERROR)
-	{
-		printf("Failed to send message: %d\n", WSAGetLastError());
-		closesocket(s_socket);
-		WSACleanup(); // Cleanup after error
-	}
-
-};
-
-void Client::checkReceiveError(int ec) {
-
-	if (ec == SOCKET_ERROR)
-	{
-		printf("Failed to receive message: %d\n", WSAGetLastError());
-		closesocket(s_socket);
-		WSACleanup(); // Cleanup after error
-	}
-
-};
-
-void Client::createSocket() {
-	
-	s_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (s_socket == INVALID_SOCKET)
+	if (s == INVALID_SOCKET) 
 	{
 		printf("Couldn't create Socket: %d\n", WSAGetLastError());
-		closesocket(s_socket);
+		closesocket(s);
 		WSACleanup(); // Cleanup after error
-		exit(1);
+		return 1;
 	}
 
-};
+	// Initalizing SOCKADDR_IN and trying to connect
 
-void Client::connectToServer() {
+	SOCKADDR_IN addr;
+	long ec; // errorcode
 
 	memset(&addr, 0, sizeof(SOCKADDR_IN)); // All 0's
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(PORT); // Host-to-Network-short function
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Change to Serveraddress later
-	FIRST_CONTACT = "type/.../Cuda\n";
-	iResult = connect(s_socket, (SOCKADDR*)&addr, sizeof(SOCKADDR));
+	addr.sin_addr.s_addr = inet_addr("localhost"); // Change to Serveraddress later
 
-	if (iResult == SOCKET_ERROR) //ERROR-Handling
+	ec = connect(s, (SOCKADDR*)&addr, sizeof(SOCKADDR));
+	if (ec == SOCKET_ERROR) 
 	{
 		printf("Failed to connect to socket: %d\n", WSAGetLastError());
-		closesocket(s_socket);
+		closesocket(s);
 		WSACleanup(); // Cleanup after error
-		exit(1);
+		return 1;
 	}
 
-	iResult = send(s_socket, FIRST_CONTACT, strlen(FIRST_CONTACT), 0);
-	checkSendError(iResult);
-
-	iResult = recv(s_socket, recvbuf, BUFLEN, 0);
-	checkReceiveError(iResult);
-};
-
-void Client::close() {
-	closesocket(s_socket);
-	WSACleanup();
+	closesocket(s);
+	WSACleanup(); // Cleanup after error
+	return 0;
 }
 
-void Client::sendMessage(const char* message) {
-	iResult = send(s_socket, message, strlen(message), 0);
-	checkSendError(iResult);
-}
 
-void Client::receiveMessage(char* receiveBuf, int len) {
-	iResult = recv(s_socket, receiveBuf, len, 0);
-	checkReceiveError(iResult);
-};
+
