aec12361222303876a7c7971bbb580fe19fe89aa
Jokiko <75935156+Jokiko@users.noreply.github.com>
2021-03-08 15:46:55 +0100

    Erfolgreiche Berechnung auf WebSockets
    Server versendet Tasks an WebSocket Client, dieser berechnet Mandelbrotmenge und sendet Ergebnis an Server zurück

--- JS Client/mandelbrot.cpp
+++ JS Client/mandelbrot.cpp
@@ -1,4 +1,4 @@
-#include <jni.h>
+
 #include <string>
 #include <fstream>      // for files manipulation
 #include <complex>      // for complex numbers
@@ -14,19 +14,15 @@ int imgPicture_getWidth;
 int yourNumber;
 int anzClients;
 
-
-int xMove, yMove = 0;
-double zoomX = 200;
-double zoomY = 200;
 double zx, zy, cx, cy, temp;
 
 
 
-int plot(int x, int y, int width, int height, int numItr){
+int plot(int x, int y, int width, int height, double xMove, double yMove, double zoom, int numItr){
  
     zx = zy = 0;
-    cx = (x - (width / 2.0) + xMove) / zoomX;
-    cy = (y - (height / 2.0) + yMove) / zoomY;
+    cx = (x - (width / 2.0) + xMove) / zoom;
+    cy = (y - (height / 2.0) + yMove) / zoom;
     // jeweils Division mit 2, damit in der Mitte des Bildschirms
     int itr = numItr;
     while (zx * zx + zy * zy < 4 && itr > 0) {
